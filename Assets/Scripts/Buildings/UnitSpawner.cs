af21700cd66f6709ebefde52f36f331df3d25bd4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-11-19 14:54:48 +0100

    Allow player to spawn units

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,42 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Buildings
+{
+    public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private GameObject unitPrefab;
+        [SerializeField] private Transform unitSpawnPoint;
+
+        #region Server
+
+        [Command]
+        private void CmdSpawnUnit()
+        {
+            var unitInstance = Instantiate(
+                unitPrefab,
+                unitSpawnPoint.position,
+                unitSpawnPoint.rotation);
+
+            NetworkServer.Spawn(unitInstance, connectionToClient);
+        }
+
+        #endregion
+
+        #region Client
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            if (!hasAuthority)
+                return;
+
+            CmdSpawnUnit();
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,9 +1,24 @@
 ﻿using Mirror;
+using UnityEngine;
 
 namespace Networking
 {
     public class RTSNetworkManager : NetworkManager
     {
+        [SerializeField] private GameObject unitSpawnerPrefab;
 
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+            var addedPlayer = conn.identity.transform;
+
+            var unitSpawnerInstance = Instantiate(
+                unitSpawnerPrefab,
+                addedPlayer.position,
+                addedPlayer.rotation);
+
+            NetworkServer.Spawn(unitSpawnerInstance, conn);
+        }
     }
 }
